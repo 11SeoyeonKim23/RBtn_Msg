5506aea6a3e83735ce055028420e385ee63c5e41
Seoyeon Kim <catneru@naver.com>
2019-05-20 09:49:13 +0900

    #2 라디오버튼 선택에 따른 메시지박스 스타일이 변하도록 코드 구현

--- RBtn_Msg/Form1.Designer.cs
+++ RBtn_Msg/Form1.Designer.cs
@@ -28,13 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gpOption1 = new System.Windows.Forms.GroupBox();
+            this.gpOption2 = new System.Windows.Forms.GroupBox();
+            this.rbOk = new System.Windows.Forms.RadioButton();
+            this.rbOkCancle = new System.Windows.Forms.RadioButton();
+            this.rbYesNo = new System.Windows.Forms.RadioButton();
+            this.rbError = new System.Windows.Forms.RadioButton();
+            this.rbInformation = new System.Windows.Forms.RadioButton();
+            this.rbQuestion = new System.Windows.Forms.RadioButton();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.gpOption1.SuspendLayout();
+            this.gpOption2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gpOption1
+            // 
+            this.gpOption1.Controls.Add(this.rbYesNo);
+            this.gpOption1.Controls.Add(this.rbOkCancle);
+            this.gpOption1.Controls.Add(this.rbOk);
+            this.gpOption1.Location = new System.Drawing.Point(12, 12);
+            this.gpOption1.Name = "gpOption1";
+            this.gpOption1.Size = new System.Drawing.Size(148, 89);
+            this.gpOption1.TabIndex = 0;
+            this.gpOption1.TabStop = false;
+            this.gpOption1.Text = "타입";
+            this.gpOption1.Enter += new System.EventHandler(this.GpOption1_Enter);
+            // 
+            // gpOption2
+            // 
+            this.gpOption2.Controls.Add(this.rbError);
+            this.gpOption2.Controls.Add(this.rbInformation);
+            this.gpOption2.Controls.Add(this.rbQuestion);
+            this.gpOption2.Location = new System.Drawing.Point(176, 12);
+            this.gpOption2.Name = "gpOption2";
+            this.gpOption2.Size = new System.Drawing.Size(142, 89);
+            this.gpOption2.TabIndex = 1;
+            this.gpOption2.TabStop = false;
+            this.gpOption2.Text = "아이콘";
+            // 
+            // rbOk
+            // 
+            this.rbOk.AutoSize = true;
+            this.rbOk.Location = new System.Drawing.Point(6, 20);
+            this.rbOk.Name = "rbOk";
+            this.rbOk.Size = new System.Drawing.Size(38, 16);
+            this.rbOk.TabIndex = 0;
+            this.rbOk.TabStop = true;
+            this.rbOk.Text = "Ok";
+            this.rbOk.UseVisualStyleBackColor = true;
+            // 
+            // rbOkCancle
+            // 
+            this.rbOkCancle.AutoSize = true;
+            this.rbOkCancle.Location = new System.Drawing.Point(6, 42);
+            this.rbOkCancle.Name = "rbOkCancle";
+            this.rbOkCancle.Size = new System.Drawing.Size(78, 16);
+            this.rbOkCancle.TabIndex = 1;
+            this.rbOkCancle.TabStop = true;
+            this.rbOkCancle.Text = "OkCancle";
+            this.rbOkCancle.UseVisualStyleBackColor = true;
+            // 
+            // rbYesNo
+            // 
+            this.rbYesNo.AutoSize = true;
+            this.rbYesNo.Location = new System.Drawing.Point(6, 64);
+            this.rbYesNo.Name = "rbYesNo";
+            this.rbYesNo.Size = new System.Drawing.Size(61, 16);
+            this.rbYesNo.TabIndex = 3;
+            this.rbYesNo.TabStop = true;
+            this.rbYesNo.Text = "YesNo";
+            this.rbYesNo.UseVisualStyleBackColor = true;
+            // 
+            // rbError
+            // 
+            this.rbError.AutoSize = true;
+            this.rbError.Location = new System.Drawing.Point(6, 20);
+            this.rbError.Name = "rbError";
+            this.rbError.Size = new System.Drawing.Size(50, 16);
+            this.rbError.TabIndex = 6;
+            this.rbError.TabStop = true;
+            this.rbError.Text = "Error";
+            this.rbError.UseVisualStyleBackColor = true;
+            // 
+            // rbInformation
+            // 
+            this.rbInformation.AutoSize = true;
+            this.rbInformation.Location = new System.Drawing.Point(6, 42);
+            this.rbInformation.Name = "rbInformation";
+            this.rbInformation.Size = new System.Drawing.Size(85, 16);
+            this.rbInformation.TabIndex = 5;
+            this.rbInformation.TabStop = true;
+            this.rbInformation.Text = "Information";
+            this.rbInformation.UseVisualStyleBackColor = true;
+            // 
+            // rbQuestion
+            // 
+            this.rbQuestion.AutoSize = true;
+            this.rbQuestion.Location = new System.Drawing.Point(6, 64);
+            this.rbQuestion.Name = "rbQuestion";
+            this.rbQuestion.Size = new System.Drawing.Size(73, 16);
+            this.rbQuestion.TabIndex = 4;
+            this.rbQuestion.TabStop = true;
+            this.rbQuestion.Text = "Question";
+            this.rbQuestion.UseVisualStyleBackColor = true;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(331, 12);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(130, 88);
+            this.btnShow.TabIndex = 2;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.BtnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(473, 110);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.gpOption2);
+            this.Controls.Add(this.gpOption1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "메시지 박스 보기";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gpOption1.ResumeLayout(false);
+            this.gpOption1.PerformLayout();
+            this.gpOption2.ResumeLayout(false);
+            this.gpOption2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gpOption1;
+        private System.Windows.Forms.RadioButton rbYesNo;
+        private System.Windows.Forms.RadioButton rbOkCancle;
+        private System.Windows.Forms.RadioButton rbOk;
+        private System.Windows.Forms.GroupBox gpOption2;
+        private System.Windows.Forms.RadioButton rbError;
+        private System.Windows.Forms.RadioButton rbInformation;
+        private System.Windows.Forms.RadioButton rbQuestion;
+        private System.Windows.Forms.Button btnShow;
     }
 }
 
--- RBtn_Msg/Form1.cs
+++ RBtn_Msg/Form1.cs
@@ -11,9 +11,49 @@ namespace RBtn_Msg
 {
     public partial class Form1 : Form
     {
+        private MessageBoxButtons mbb;
+        private MessageBoxIcon mbi;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void GpOption1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnShow_Click(object sender, EventArgs e)
+        {
+            if (this.rbOk.Checked)
+            {
+                this.mbb = MessageBoxButtons.OK;
+            }
+            else if (this.rbOkCancle.Checked)
+            {
+                this.mbb = MessageBoxButtons.OKCancel;
+            }
+            else if (this.rbYesNo.Checked)
+            {
+                this.mbb = MessageBoxButtons.YesNo;
+            }
+            else if (this.rbError.Checked)
+            {
+                this.mbb = MessageBoxIcon.Error;
+            }
+            else if (this.rbInformation.Checked)
+            {
+                this.mbb = MessageBoxIcon.Information;
+            }
+            else if (this.rbQuestion.Checked)
+            {
+                this.mbb = MessageBoxIcon.Question;
+            }
         }
     }
 }
